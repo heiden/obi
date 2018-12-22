0c075d46a3a15835ffad3b1111647d6761e8cf5e
stand up guy <43141586+limecookpiss@users.noreply.github.com>
2018-12-22 18:20:55 -0200

    consulta pronta

--- consulta.php
+++ consulta.php
@@ -4,6 +4,7 @@
 <head>
 	<title> obi </title>
 	<meta charset = "utf-8">
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<link rel="stylesheet" type="text/css" href="obi.css">
 	<!-- <script type="text/javascript" src="obi.js"></script> -->
 
@@ -31,8 +32,17 @@
 
 	<center>
 
+	<table>
+	  		<tr>
+			    <th>Problema</th>
+	    		<th>Nível</th>
+	    		<th>Tag</th>
+	    		<th>Ano</th>
+	    		<th>PDF</th>
+	  		</tr>
+
 	<?php 
-	
+
 	$ano = $_POST["ano"];
 	$tag = $_POST["tag"];
 	$nome = $_POST["nome"];
@@ -42,6 +52,8 @@
 	if (!$link )
 		die('Não foi possível conectar: ' . mysql_error());
 	
+	// converter charset do mysql pra utf8
+	mysqli_set_charset($link, "utf8");
 	
 	// echo $nome;
 
@@ -69,26 +81,24 @@
 		if($ano != "")
 			$query .= "ano = '$ano' ";
 
-		$query .= ";";
+		$query .= "ORDER BY nome;";
 
-		echo $query;
 	}
 	else{
 		print("Você precisa inserir pelo menos um termo de busca!");
 	}
 
 	$result = mysqli_query($link, $query);
-	// $num = 0;
 
 	while ($row = mysqli_fetch_row($result)) {
-		// $num++;
-
 		// $id = $row[0];
-		$nome = $row[1];
-		// $arq = $row[2];
-		$nivel = $row[3];
-		$tag = $row[4];
+		$nome = $row[1]; 	$nome[0] = strtoupper($nome[0]);
+		$arq = $row[2];
+		$nivel = $row[3]; 	$nivel[0] = strtoupper($nivel[0]);
+		$tag = $row[4];		$tag[0] = strtoupper($tag[0]);
 		$ano = $row[5];
+
+		$pdf = "questao.php?questao=" . $arq;
 
 		echo	"
 				<tr>
@@ -96,12 +106,15 @@
 		    		<td> $nivel </td>
 			    	<td> $tag </td>
 			    	<td> $ano </td>
+			    	<td> <a href = $pdf><img src = 'pdf.png' width='30' height='30'> </a> </td>
 	  			</tr>
 	  			";
 
 	}
 
 	?>
+
+	</table>
 
 	</center>
 
